83698ed7787c6d13dcb6c68af3600fd56272fb99
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-08-14 01:17:42 +0900

    Build Listing Details Screen

--- App.js
+++ App.js
@@ -4,28 +4,30 @@ import WelcomeScreen from './app/screens/WelcomeScreen'
 import ViewImageScreen from './app/screens/ViewImageScreen'
 import AppButton from './app/components/AppButton'
 import Card from './app/components/Card'
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
 
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: '#f8f4f4',
-        flex: 1,
-        padding: 20,
-        paddingTop: 60,
-      }}
-    >
-      <Card
-        title="Red jacket for Sale!"
-        subTitle="$100"
-        image={require('./app/assets/jacket.jpg')}
-      />
-      <Card
-        title="Red jacket for Sale!"
-        subTitle="$100"
-        image={require('./app/assets/jacket.jpg')}
-      />
-    </View>
+    // <View
+    //   style={{
+    //     backgroundColor: '#f8f4f4',
+    //     flex: 1,
+    //     padding: 20,
+    //     paddingTop: 60,
+    //   }}
+    // >
+    //   <Card
+    //     title="Red jacket for Sale!"
+    //     subTitle="$100"
+    //     image={require('./app/assets/jacket.jpg')}
+    //   />
+    //   <Card
+    //     title="Red jacket for Sale!"
+    //     subTitle="$100"
+    //     image={require('./app/assets/jacket.jpg')}
+    //   />
+    // </View>
+    <ListingDetailsScreen />
   )
 }
 
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Image, StyleSheet } from 'react-native'
+
+import AppText from '../components/AppText'
+import colors from '../config/colors'
+
+const ListingDetailsScreen = () => {
+  return (
+    <View>
+      <Image source={require('../assets/jacket.jpg')} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for Sale!</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+})
+
+export default ListingDetailsScreen

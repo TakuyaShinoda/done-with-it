cf348b1e4e15f4adca7a944713d12e5570a74f02
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-08-30 12:43:41 +0900

    Implement Pull to Refresh

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -4,6 +4,7 @@ import { FlatList, StyleSheet, View } from 'react-native'
 import ListItem from '../components/ListItem'
 import ListItemSeparator from '../components/ListItemSeparator'
 import ListItemDeleteAction from '../components/ListItemDeleteAction'
+import Screen from '../components/Secreen'
 
 const initialMessages = [
   {
@@ -22,28 +23,42 @@ const initialMessages = [
 
 const MessagesScreen = () => {
   const [messages, setMessages] = useState(initialMessages)
+  const [refreshing, setRefreshing] = useState(false)
 
   const handleDelete = message => {
     setMessages(messages.filter(m => m.id !== message.id))
   }
 
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={messages => messages.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-          onPress={() => console.log(item.title)}
-          renderRightActions={() => (
-            <ListItemDeleteAction onPress={() => handleDelete(item)} />
-          )}
-        />
-      )}
-      ItemSeparatorComponent={ListItemSeparator}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={messages => messages.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log(item.title)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 1,
+              title: 'T1',
+              description: 'D1',
+              image: require('../assets/mosh.jpg'),
+            },
+          ])
+        }}
+      />
+    </Screen>
   )
 }
 

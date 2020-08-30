f5ef2030d1fb916b09bd6c6dc1338640d9aea527
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-08-30 14:55:52 +0900

    Build TextInput Component

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import ListingsScreen from './app/screens/ListingsScreen'
+import React, { useState } from 'react'
+import { Text, TextInput } from 'react-native'
+
+import Screen from './app/components/Screen'
+import colors from './app/config/colors'
+import AppTextInput from './app/components/AppTextInput'
 
 const App = () => {
-  return <ListingsScreen />
+  const [firstName, setFirstName] = useState('')
+
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  )
 }
 
 export default App

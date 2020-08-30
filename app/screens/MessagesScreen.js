afd669195e7b18633ef863115682229cbf530d48
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-08-30 12:19:08 +0900

    Implement function of Deleting Message

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
 
 import ListItem from '../components/ListItem'
 import ListItemSeparator from '../components/ListItemSeparator'
 import ListItemDeleteAction from '../components/ListItemDeleteAction'
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: 'T1',
@@ -21,6 +21,12 @@ const messages = [
 ]
 
 const MessagesScreen = () => {
+  const [messages, setMessages] = useState(initialMessages)
+
+  const handleDelete = message => {
+    setMessages(messages.filter(m => m.id !== message.id))
+  }
+
   return (
     <FlatList
       data={messages}
@@ -31,7 +37,9 @@ const MessagesScreen = () => {
           subTitle={item.description}
           image={item.image}
           onPress={() => console.log(item.title)}
-          renderRightActions={ListItemDeleteAction}
+          renderRightActions={() => (
+            <ListItemDeleteAction onPress={() => handleDelete(item)} />
+          )}
         />
       )}
       ItemSeparatorComponent={ListItemSeparator}

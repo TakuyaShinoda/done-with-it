e70139184f6cbf2370f6a9fcc3c2f9d770a0c20e
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-09-01 22:58:56 +0900

    Request Permission & Access Cameraroll

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Image } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 
-import MessagesScreen from './app/screens/MessagesScreen'
-import ListingEditScreen from './app/screens/ListingEditScreen'
+import Screen from './app/components/Screen'
 
 const App = () => {
-  return <ListingEditScreen />
+  const [imageUri, setImageUri] = useState()
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync()
+    if (!granted) alert('You need to enable permission to access te library.')
+  }
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.cancelled) setImageUri(result.uri)
+    } catch (error) {
+      console.log('Error reading an Image', error)
+    }
+  }
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  )
 }
 
 export default App

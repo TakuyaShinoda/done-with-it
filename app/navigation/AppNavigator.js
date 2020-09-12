2c4ba84b743918d1f2b4f2cab070ce10f418b181
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-09-12 21:14:22 +0900

    Beautify Tabs

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import ListingEditScreen from '../screens/ListingEditScreen'
-import AccountScreen from '../screens/AccountScreen'
+import AccountNavigator from './AccountNavigator'
 import FeedNavigator from './FeedNavigation'
+import NewListingButton from './NewListingButton'
 
 const Tab = createBottomTabNavigator()
 
 const AppNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Feed" component={FeedNavigator} />
-      <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Screen
+        name="Feed"
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ListingEdit"
+        component={ListingEditScreen}
+        options={({ navigation }) => ({
+          tabBarButton: () => (
+            <NewListingButton
+              onPress={() => {
+                navigation.navigate('ListingEdit')
+              }}
+            />
+          ),
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="plus-circle"
+              size={size}
+              color={color}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }

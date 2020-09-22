41a1bc8518638d5a416f756434ffb81ba861ac36
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-09-22 18:17:43 +0900

    Reset Form after Posting

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -91,18 +91,21 @@ const ListingEditScreen = () => {
   const [uploadVisible, setUploadVisible] = useState(false)
   const [progress, setProgress] = useState(0)
 
-  const handleSubmit = async listing => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0)
     setUploadVisible(true)
     const result = await listingsApi.addListing(
       { ...listing, location },
       progress => setProgress(progress)
     )
+    console.log(result)
 
-    if (!result.ok) {
-      setUploadVisible(false)
-      return alert('Could not save the listing.')
-    }
+    // if (!result.ok) {
+    //   setUploadVisible(false)
+    //   return alert('Could not save the listing.')
+    // }
+
+    resetForm()
   }
 
   return (
